eb66739a04f7f600cc32764b193743c2ded9efb5
GnomeyDev <106401515+GnomeyDev@users.noreply.github.com>
2022-06-01 20:32:37 +0100

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using Memory;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace HelixClient
+{
+    public partial class Form1 : Form
+    {
+
+        Memory.Mem memory = new Memory.Mem();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void airjump_CheckedChanged(object sender, EventArgs e)
+        {
+            while (airjump.Checked);
+                    {
+                        memory.OpenProcess(Process.GetProcessesByName("Minecraft.Windows").FirstOrDefault().Id);
+                        memory.WriteMemory("Minecraft.Windows.exe + 0549E7E8,8,18,b8,1D8", "4", "16777473");
+                    }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Helix-Client
+A Minecraft Bedrock Edition Utility Client (This is my first so it will be buggy)
+
+# Current Features
+- AirJump
+- Fov
+- Y Booster
+
+# Licence For Dummys
+
+Use the FORK feature it will help you alot more when using our code
+
+When using the code
+- Make sure to credit us
+- Use the same licence
+- Give us permission to use your code in our codebase
+- Make sure it is public
+- Make sure its know you are using our code
+
+# Credits
+Developer - Wuuck#4431
+
+# Discord Server
+https://discord.gg/z3kJbm7Sbk
